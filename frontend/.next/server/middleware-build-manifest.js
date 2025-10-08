162bc0e4fa2a61a5a74b1f62bf267428ac60c7c7
Rohit Kumar Sah <rohit.sah@op.iitg.ac.in>
2025-10-08 18:07:01 +0530

    .next folder and env

---
+++ frontend/.next/server/middleware-build-manifest.js
@@ -0,0 +1,34 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": [
+      "static/chunks/e60ef129113f6e24.js",
+      "static/chunks/560f5d10a2888188.js",
+      "static/chunks/turbopack-ed0446024c2125d3.js"
+    ],
+    "/_error": [
+      "static/chunks/17722e3ac4e00587.js",
+      "static/chunks/560f5d10a2888188.js",
+      "static/chunks/turbopack-0edd383c097e564e.js"
+    ]
+  },
+  "devFiles": [],
+  "ampDevFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/aaea1e64ef7e0faa.js",
+    "static/chunks/91adb7bdb9870c6a.js",
+    "static/chunks/30cb146bc1e6f45f.js",
+    "static/chunks/8082ab48faca5ea1.js",
+    "static/chunks/8de370c3cbabf345.js",
+    "static/chunks/turbopack-c7245055f1bf453d.js"
+  ],
+  "ampFirstPages": []
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
---
+++ frontend/.next/static/chunks/d708ade73149821c.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/560f5d10a2888188.js",
+  "static/chunks/turbopack-0edd383c097e564e.js"
+])
